5938e00a893746213ff89cfca51b6c50563e30df
p.bykov <p.bykov@qiwi.com>
2024-11-19 16:35:07 +0300

    fix input data

--- addgitpush.bash
+++ addgitpush.bash
@@ -2,13 +2,13 @@
 if [ -e ./.git ]; then
     STR=$(cat ./.git/HEAD)
     git add .
-    git commit -m "$1"
+    git commit -m "$@"
     git pull -r
     git push origin HEAD:refs/for/${STR:16}
 elif [ $# -eq 0 ]; then
     echo "No argument"
     exit 1
 else
-    echo "You are not in the right directory or there is no branch $1"
+    echo "You are not in the right directory or there is no branch $@"
     exit 0
 fi
